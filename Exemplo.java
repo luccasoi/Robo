047c04ae2f41ce82e63ece2ef200716f4651937d
luccas <luccaspazzanese@gmail.com>
2025-04-30 01:02:20 -0300

    trabalho robo

---
+++ Exemplo.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao,gg;
+        int TENTAIVAS_MAXIMAS = 7;
+        double valor1, valor2, posicao;
+  
+        System.out.println("1 para frente");
+        System.out.println("2 para direita");
+        System.out.println("3 para esquerda");
+        System.out.println("4 para tras");
+                
+        for (gg = TENTAIVAS_MAXIMAS; gg >= 0; gg--){
+            System.out.println("Direção que deseja andar");
+            opcao = sc.nextInt();
+            if ((opcao < 1) || (opcao>4)){
+                System.out.println("❌Valor invalido❌");
+                break;
+            }
+
+            System.out.println("Quantos que deseja andar");
+            valor2 = sc.nextDouble();
+                
+            if(valor2 < 0 && valor2 > 10){
+                System.out.println("nao é possivel");
+            } 
+
+            if (opcao == 1) {
+                posicao = valor2;
+                System.out.println("Andou para frente na quantidade " + posicao + " passos");
+            }
+            if (opcao == 2) {
+                posicao = valor2;
+                System.out.println("Andou para direita na quantidade " + posicao + " passos");
+            }
+            if (opcao == 3) {
+                posicao = valor2;
+                System.out.println("Andou para esquerda na quantidade " + posicao + " passos");
+            }
+            if (opcao == 4) {
+                posicao = valor2;
+                System.out.println("Andou para tras na quantiade " + posicao + " passos");
+            }
+            System.out.println("✅Comando Valido✅");
+
+            if(gg == 0){
+                System.out.println("⚠️Tentativas Maximas Concluidas⚠️");
+            }
+        }
+    }
+}
+
+// javac <arquivo.java>
+// java arquivo
