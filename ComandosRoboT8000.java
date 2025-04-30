716ccf4b263f6e38233d719a1154fd1de33d31c1
luccas <luccaspazzanese@gmail.com>
2025-04-30 01:13:18 -0300

    alteracao feita

--- ComandosRoboT8000.java
+++ ComandosRoboT8000.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Exemplo {
+public class ComandosRoboT8000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcao,gg;
         int TENTAIVAS_MAXIMAS = 7;
         double valor1, valor2, posicao;
-  
+
         System.out.println("1 para frente");
         System.out.println("2 para direita");
         System.out.println("3 para esquerda");
         System.out.println("4 para tras");
-                
+
         for (gg = TENTAIVAS_MAXIMAS; gg >= 0; gg--){
             System.out.println("Direção que deseja andar");
             opcao = sc.nextInt();
@@ -20,13 +20,13 @@ public class Exemplo {
                 break;
             }
 
-            System.out.println("Quantos que deseja andar");
+            System.out.println("Quantos passos que deseja andar");
             valor2 = sc.nextDouble();
-                
-            if(valor2 < 0 && valor2 > 10){
-                System.out.println("nao é possivel");
-            } 
 
+            if(valor2 < 0 || valor2 > 10){
+                System.out.println("❌Valor invalido❌");
+                break;
+            }
             if (opcao == 1) {
                 posicao = valor2;
                 System.out.println("Andou para frente na quantidade " + posicao + " passos");
